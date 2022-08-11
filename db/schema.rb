bb5b0120d878433c41b683ac03ee55f35ec700cc
nhuhoacn <nhuhoacn@gmail.com>
2022-08-11 14:04:01 +0700

    signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_160424) do
+ActiveRecord::Schema.define(version: 2022_08_10_081931) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2022_08_08_160424) do
     t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "images", "products"
